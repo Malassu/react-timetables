b968e2f73b020672fe6ea6080987a996b43fdcac
Ilkka Malassu <ilkka.malassu@aalto.fi>
2020-01-25 14:41:19 +0200

    Add transportation ID and visual changes

--- react-hsl/src/App.js
+++ react-hsl/src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import './App.css';
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function wait() {
-  await sleep(5000);
-}
+function lowerCase(string) {
+  return string.charAt(0) + string.slice(1).toLowerCase();
+};
 
 class App extends React.Component {
   constructor(props) {
@@ -15,8 +11,10 @@ class App extends React.Component {
     this.state = {
       start: [],
       startTimes: [],
+      destinations: [],
       next: [],
-      nextTimes: []
+      nextTimes: [],
+      transIDs: []
     };
   }
   
@@ -67,26 +65,31 @@ class App extends React.Component {
       var startTimes = [];
       var next = [];
       var nextTimes = [];
+      var destinations = [];
+      var transIDs = [];
       
       for(var i = 0; i < itineraries.length; i++) {
-        start.push(itineraries[i].legs[0].mode);
+        start.push(lowerCase(itineraries[i].legs[0].mode));
         var date = new Date(itineraries[i].legs[0].startTime);
         var hours = date.getHours();
         var minutes = "0" + date.getMinutes();
         startTimes.push(hours + ':' + minutes.substr(-2));
+        destinations.push(itineraries[i].legs[0].to.name);
         if(itineraries[i].legs.length > 1) {
-          next.push(itineraries[i].legs[1].mode);
+          next.push(lowerCase(itineraries[i].legs[1].mode));
           date = new Date(itineraries[i].legs[1].startTime);
           hours = date.getHours();
           minutes = "0" + date.getMinutes();
-          nextTimes.push(hours + ':' + minutes.substr(-2))
+          nextTimes.push(hours + ':' + minutes.substr(-2));
+          transIDs.push(itineraries[i].legs[1].trip.routeShortName + ', ' + itineraries[i].legs[1].trip.tripHeadsign);
         }
         else {
           next.push('End');
           nextTimes.push('End');
+          transIDs.push('End');
         }
       }
-      this.setState({start: start, startTimes: startTimes, next: next, nextTimes: nextTimes});
+      this.setState({start: start, startTimes: startTimes, destinations: destinations, next: next, nextTimes: nextTimes, transIDs: transIDs});
 
     });
   };
@@ -97,25 +100,31 @@ class App extends React.Component {
       var columns = [];
       columns.push(<td>{this.state.startTimes[i]}</td>);
       columns.push(<td>{this.state.start[i]}</td>);
+      columns.push(<td>{this.state.destinations[i]}</td>);
       columns.push(<td>{this.state.nextTimes[i]}</td>);
       columns.push(<td>{this.state.next[i]}</td>);
+      columns.push(<td>{this.state.transIDs[i]}</td>);
       table.push(<tr>{columns}</tr>);
     }
     return table;
   }
 
   render () {
-    wait();
     this.get_timetables();
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes();
     return (
       <div className="App">
         <h1>Timetables from Eficode office to Kumpula</h1>
+        <h2>Current time: {time}</h2>
         <table align="center">
           <tr>
             <th>Leave work</th>
             <th>Method</th>
+            <th>Destination</th>
             <th>Transportation time</th>
             <th>Transportation method</th>
+            <th>Transportation ID</th>
           </tr>
           {this.createTable()}
         </table>
